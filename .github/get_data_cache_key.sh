90899f464b74672458ee125b7f4d3460026e9ae7
Christian Johansen <christian.johansen@mattilsynet.no>
2026-05-11 12:43:02 +0200

    Lag cache-nøkkel ut fra de dataene vi bruker
    ...ikke fra en server som ikke lenger finnes

--- .github/get_data_cache_key.sh
+++ .github/get_data_cache_key.sh
@@ -4,7 +4,4 @@ get_etag() {
     curl -sI "$1" | grep -i etag | awk '{print $2}' | tr -d '"' | tr -d '[:space:]'
 }
 
-tilsyn_etag=$(get_etag "https://data.mattilsynet.no/smilefjes-tilsyn.csv")
-kravpunkter_etag=$(get_etag "https://data.mattilsynet.no/smilefjes-kravpunkter.csv")
-
-echo "tilsyn_$tilsyn_etag/kravpunkter_$kravpunkter_etag"
+get_etag "https://matnyttig.mattilsynet.no/api/nyeste-dato"
